4017b4aa0eb5d05181fb593e6537f0591b32ca8e
Slaviša Simić <slavisasimic3@gmail.com>
2025-06-20 15:47:46 +0200

    EP-2: Implementiran entitet Device sa jedinstvenim identifikatorom i logikom automatskog merenja i slanja temperature regulatoru na svakih 3 minuta. Testirano inicijalizovanjem uređaja i ispisom podataka.

--- Application/Interfaces/IDeviceService.cs
+++ Application/Interfaces/IDeviceService.cs
@@ -7,6 +7,17 @@ namespace Application.Interfaces
 {
     public interface IDeviceService
     {
-        
+        IEnumerable<Device> GetAllDevices();
+        void UpdateTemperature(string deviceId, double newTemperature);
+        Device? GetDeviceById(string deviceId);
+
+        void StartAllDevices(CancellationToken cancellationToken = default);
+        void StopAllDevices();
+
+        void StartHeatingAll();
+        void StopHeatingAll();
+
+        void SubscribeToTemperatureEvents(Action<string, double> temperatureHandler);
+        void UnsubscribeFromTemperatureEvents(Action<string, double> temperatureHandler);
     }
 }
---
+++ Presentation/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Domain;
+
+namespace Presentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kreiramo listu uređaja
+            var devices = new List<Device>
+            {
+                new Device("1", 22.0),
+                new Device("2", 20.5),
+                new Device("3", 19.0),
+                new Device("4", 21.3)
+            };
+
+            // Ispisujemo podatke o uređajima
+            Console.WriteLine("Pregled registrovanih uređaja:");
+            foreach (var device in devices)
+            {
+                Console.WriteLine($"ID: {device.Id}, Trenutna temperatura: {device.CurrentTemperature}°C");
+            }
+
+            // Pauza od 3 minuta (10 sekundi za brzi test)
+            Console.WriteLine("\nČekam 3 minuta (ili skrati na 10 sekundi za brži test)...");
+            Thread.Sleep(TimeSpan.FromSeconds(10));
+
+            // Simulacija slanja temperature regulatoru
+            foreach (var device in devices)
+            {
+                Console.WriteLine($"Uređaj {device.Id}: uspešno poslata temperatura {device.CurrentTemperature}°C regulatoru.");
+            }
+
+            Console.WriteLine("\nTest EP-2 uspešno završen.");
+        }
+    }
+}
